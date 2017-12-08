c3fe7905b2c1fc5bd72402d66d2ddafba7431407
yuyin <lytforgood@163.com>
2017-12-08 10:32:06 +0800

    add

--- README.md
+++ README.md
@@ -2,12 +2,11 @@ Machine Learning Trick : GBDT_Feature Blending Stacking CascadeForest
 ---
 
 1. xgboost调参演示.md 
-2. 机器学习算法实践.md  GBDT XGBOOST参数解析+网格搜索
-3. Xgboost_Feature.py  利用Xgboost的叶子节点位置生成新特征封装的函数
-4. xgboost生成新特征.md  利用Xgboost的叶子节点位置生成新特征-函数使用和具体过程
-4. Blending.py 
-5. Stacking.py
-6. CascadeForest.py
+2. 机器学习算法实践.md       ---GBDT XGBOOST参数解析+网格搜索
+3. Xgboost_Feature.py     ---利用Xgboost的叶子节点位置生成新特征封装的函数
+4. xgboost生成新特征.md     ---利用Xgboost的叶子节点位置生成新特征-函数使用和具体过程
+4. Blending.py [参考](http://blog.csdn.net/a358463121/article/details/53054686) [示例](https://github.com/emanuele/kaggle_pbr/blob/master/blend.py)
+5. Stacking.py [参考](https://github.com/MLWave/Kaggle-Ensemble-Guide)
 
 
 
---
+++ Stacking.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from heamy.dataset import Dataset
+from heamy.estimator import Regressor, Classifier
+from heamy.pipeline import ModelsPipeline
+from sklearn import cross_validation
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+#加载数据集
+from sklearn.datasets import load_boston
+data = load_boston()
+X, y = data['data'], data['target']
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.1, random_state=111)
+#创建数据集
+dataset = Dataset(X_train,y_train,X_test)
+#创建RF模型和LR模型
+model_rf = Regressor(dataset=dataset, estimator=RandomForestRegressor, parameters={'n_estimators': 50},name='rf')
+model_lr = Regressor(dataset=dataset, estimator=LinearRegression, parameters={'normalize': True},name='lr')
+# Stack两个模型
+# Returns new dataset with out-of-fold predictions
+pipeline = ModelsPipeline(model_rf,model_lr)
+stack_ds = pipeline.stack(k=10,seed=111)
+#第二层使用lr模型stack
+stacker = Regressor(dataset=stack_ds, estimator=LinearRegression)
+results = stacker.predict()
+# 使用10折交叉验证结果
+results10 = stacker.validate(k=10,scorer=mean_absolute_error)
